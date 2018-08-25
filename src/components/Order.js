a8645901a2339d1fd6b7c11c058de106b922056c
Josh Silverman <jrsilverman71@hotmail.com>
2018-08-24 17:02:59 -0700

    setting up state in app

---
+++ src/components/Order.js
@@ -0,0 +1,14 @@
+import React from "react";
+ class Order extends React.Component {
+     render () {
+        return (
+           <div className="Order">
+            Order!!
+            </div>
+          
+        );
+
+     }
+ }
+
+ export default Order;
