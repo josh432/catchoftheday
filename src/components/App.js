9c5c0992577aa8d509529216397b4dd4cd3d0bef
Josh Silverman <jrsilverman71@hotmail.com>
2018-08-27 14:51:58 -0700

    local storage

--- src/components/App.js
+++ src/components/App.js
@@ -12,16 +12,30 @@ class App extends React.Component {
     };
 
     componentDidMount() {
-        const { params } = this.props.match; 
+        const { params } = this.props.match;
+        const localStorageRef = localStorage.getItem(params.storeId); 
+        if (localStorageRef) {
+            this.setState({ order: JSON.parse(localStorageRef)})
+        }
        this.ref = base.syncState(`${params.storeId}/fishes`, {
            context: this,
            state: 'fishes'
        }); 
     }
 
+    componentDidUpdate() {
+       console.log(this.state.order);
+       localStorage.setItem(
+        this.props.match.params.storeId, 
+        JSON.stringify(this.state.order)
+       );
+    }
+
     componentWillUnmount() {
         base.removeBinding(this.ref);
     }
+
+
 
     addFish = fish => {
       //1. take a copy of the existing state
